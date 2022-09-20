4c92d71c24f00fa8d0b5461b0e79d4f9d30a01d6
Matas Zabukas <mataszabukas@gmail.com>
2022-09-20 16:15:34 +0300

    Add change text color button

--- app/src/main/java/com/example/bandymas/MainActivity.java
+++ app/src/main/java/com/example/bandymas/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.bandymas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvMain = (TextView)findViewById(R.id.tvMain);
+
     }
 
     public void onBtnChangeTextClick(View view) {
-        //TextView tvMain = (TextView)findViewById(R.id.tvMain);
-        tvMain.setText("Hello");
+        tvMain.setText("Changed text!");
+    }
+
+    public void onBtnChangeTextColorClick(View view) {
+        Random rnd = new Random();
+        int randGeneratedColor = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        tvMain.setTextColor(randGeneratedColor);
     }
 }
